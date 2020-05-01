3f8a31841598e000e3d31510d5c2eb701c67eeb3
ppau0002 <ppau0002@synchronoss.com>
2020-05-01 14:44:43 +0530

    Profile created and module created for Angular Rest UI

---
+++ modules/custom/drup_angle_content_api/src/Plugin/rest/resource/GetContentTypes.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\drup_angle_content_api\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "content_type_resource",
+ *   label = @Translation("Content Type Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/contentTypes"
+ *   }
+ * )
+ */
+
+class GetContentTypes extends ResourceBase {
+        
+ /**
+   * Responds to entity GET requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  public function get() {
+    $response = ['message' => 'Hello, this is a rest service'];
+    return new ResourceResponse($response);
+  }
+}
